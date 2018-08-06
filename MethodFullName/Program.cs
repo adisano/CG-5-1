5799ff9219ae1ff26740497dd176d4b803390e58
Adriana Disano <adrianadisano@gmail.com>
2018-08-06 17:00:13 -0500

    -Created.

--- MethodFullName/Program.cs
+++ MethodFullName/Program.cs
@@ -6,7 +6,33 @@ namespace MethodFullName
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //prompt user for input
+            Console.WriteLine("Please enter your first name:");
+            //acquire user first name
+            string userNameFirst = Console.ReadLine();
+            //prompt user for input
+            Console.WriteLine("Please enter your last name:");
+            //acquire user last name
+            string userNameLast = Console.ReadLine();
+
+            //execute the method CombineNames with the parameters of userNameFirst and userNameLast
+            CombineNames(userNameFirst, userNameLast);
+
+            //keep the program open until user hits enter or closes it
+            Console.ReadLine();
+
+        }
+
+        //pass the parameters into the variables nameFirst and nameLast
+        private static string CombineNames(string nameFirst, string nameLast)
+        {
+            //concatenate the strings
+            string nameFull = nameFirst + " " + nameLast;
+            //print the result
+            Console.WriteLine(nameFull);
+            //pass the result back outside these code brackets
+            return nameFull;
+
         }
     }
 }
